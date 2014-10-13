142e42d36fa9edf3a6b4121fcf24a7325dee5cf8
Daniel Galán y Martins <git@galan.de>
2014-10-13 11:43:57 +0200

    Projekt skeleton

---
+++ README.md
@@ -0,0 +1,1 @@
+Implementation of dms-exchange-specification in java. Contains ready-to-use classes, etc. to be used in your own dms.
---
+++ src/main/java/de/galan/dmsexchange/Document.java
@@ -0,0 +1,11 @@
+package de.galan.dmsexchange;
+
+
+/**
+ * daniel should have written a comment here.
+ *
+ * @author daniel
+ */
+public class Document {
+
+}
