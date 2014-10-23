37970119ef6e97aaacfae25ed47c293784cc460c
Daniel Galán y Martins <git@galan.de>
2014-10-23 23:45:09 +0200

    later

---
+++ src/main/java/de/galan/dmsexchange/Draft.java
@@ -0,0 +1,21 @@
+package de.galan.dmsexchange;
+
+
+
+/**
+ * daniel should have written a comment here.
+ *
+ * @author daniel
+ */
+public class Draft {
+
+	public static void main(String[] args) {
+		//DocumentVersions versions = new DocumentVersions();
+		//Verjson<Document> verjson = Verjson.create(Document.class, versions);
+
+		/*
+		Export export = DmsExchange.createExport(location eg. stream,file,filename);
+		export.
+		 */
+	}
+}
