dddc6fdf833a5e5c25612766cca77e5a106ef363
Daniel Galán y Martins <git@galan.de>
2014-10-24 23:14:23 +0200

    drafting

--- src/main/java/de/galan/dmsexchange/Draft.java
+++ src/main/java/de/galan/dmsexchange/Draft.java
@@ -1,7 +1,5 @@
 package de.galan.dmsexchange;
 
-
-
 /**
  * daniel should have written a comment here.
  *
@@ -14,8 +12,23 @@ public class Draft {
 		//Verjson<Document> verjson = Verjson.create(Document.class, versions);
 
 		/*
-		Export export = DmsExchange.createExport(location eg. stream,file,filename);
-		export.
+		DmsExchange exchange = DmsExchange.create();
+		Export export = exchange.export(location eg. stream,file,filename);
+		export.addListener(listeners); // better eventbus
+		export.exportBy(email);
+		export.put(Document); // loop
+		export.split(); // ? maybe automaticallly by setting up conditions
+		export.finished(); // notifies potential listeners
+		 */
+		/*
+
+		Import import = exchange.createExport();
+		import.register(listener);
+		import.import(location eg. stream, file, filename);
+		...
+		listener.onDocument(Document document);
+		listener.afterExport(Export export);
 		 */
 	}
+
 }
